7e85cd7da001fd15c1d532266c0b9a0c4d27bcbc
Eduardo Cunha <eduardo.cunha@ist.utl.pt>
2022-07-25 12:43:53 +0100

    GenServer to handle URLs health checks
    Initially the GenServer will check if the provided URLs list is
    valid and then periodically check if a HTTP call to the URL returns
    a healthy reply.

--- lib/url_failover.ex
+++ lib/url_failover.ex
@@ -3,16 +3,52 @@ defmodule UrlFailover do
   Documentation for `UrlFailover`.
   """
 
-  @doc """
-  Hello world.
+  use GenServer
 
-  ## Examples
+  @check_interval Application.compile_env(:url_failover, :check_interval, :timer.seconds(30))
 
-      iex> UrlFailover.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start_link(opts) do
+    {name, opts} = Keyword.pop(opts, :name, __MODULE__)
+    GenServer.start_link(__MODULE__, opts, name: name)
   end
+
+  @impl true
+  def init(opts) do
+    with {:ok, urls} when is_list(urls) <- Keyword.fetch(opts, :urls),
+         true <- valid_list_if_urls?(urls) do
+      send(self(), :check)
+      :timer.send_interval(@check_interval, :check)
+
+      {:ok, %{check_urls: urls}}
+    else
+      :error ->
+        {:stop, :no_urls_provided}
+
+      _ ->
+        {:stop, :invalid_list_of_urls}
+    end
+  end
+
+  @impl true
+  def handle_info(:check, %{check_urls: _check_urls} = state) do
+    {:noreply, state}
+  end
+
+  defp valid_list_if_urls?(urls) do
+    Enum.all?(urls, &valid_url?/1)
+  end
+
+  defp valid_url?(url) when is_binary(url), do: url |> URI.parse() |> valid_uri?()
+  defp valid_url?(url) when is_struct(url, URI), do: valid_uri?(url)
+  defp valid_url?(_url), do: false
+
+  defp valid_uri?(%URI{
+         scheme: scheme,
+         host: host,
+         port: port
+       })
+       when scheme in ["http", "https"] and byte_size(host) > 0 and is_integer(port),
+       do: true
+
+  defp valid_uri?(_uri), do: false
 end
--- test/url_failover_test.exs
+++ test/url_failover_test.exs
@@ -1,8 +1,56 @@
 defmodule UrlFailoverTest do
   use ExUnit.Case
-  doctest UrlFailover
 
-  test "greets the world" do
-    assert UrlFailover.hello() == :world
+  describe "start_link/1" do
+    test "defaults process name to UrlFailover" do
+      pid = start_supervised!({UrlFailover, urls: []})
+      assert GenServer.whereis(UrlFailover) == pid
+    end
+
+    test "accepts a name for the process", %{test: test} do
+      pid = start_supervised!({UrlFailover, urls: [], name: test})
+      assert GenServer.whereis(test) == pid
+    end
+
+    test "returns {:error, :no_urls_provided} if no URLs are given", %{test: test} do
+      # using start_supervised/1 here to handle exit from star_link call which can make
+      # the test fail, also start_supervised/1 changes the format of the returned error
+      assert {:error, {:no_urls_provided, _}} = start_supervised({UrlFailover, name: test})
+    end
+
+    test "returns {:error, :invalid_list_of_urls} if at least one of the give URLs is invalid", %{
+      test: test
+    } do
+      # using start_supervised/1 here to handle exit from star_link call which can make
+      # the test fail, also start_supervised/1 changes the format of the returned error
+
+      assert {:error, {:invalid_list_of_urls, _}} =
+               start_supervised({UrlFailover, urls: [:invalid], name: test})
+
+      assert {:error, {:invalid_list_of_urls, _}} =
+               start_supervised({UrlFailover, urls: ["invalid"], name: test})
+
+      assert {:error, {:invalid_list_of_urls, _}} =
+               start_supervised({UrlFailover, urls: ["http"], name: test})
+
+      assert {:error, {:invalid_list_of_urls, _}} =
+               start_supervised({UrlFailover, urls: ["http://"], name: test})
+
+      assert {:error, {:invalid_list_of_urls, _}} =
+               start_supervised({UrlFailover, urls: ["ftp://www.google.com"], name: test})
+    end
+
+    test "accepts a list of valid URLs", %{test: test} do
+      urls = [
+        "https://www.google.com",
+        "https://www.google.com:443",
+        "http://localhost:8000",
+        "http://localhost:8000/path",
+        "http://user:pass@localhost:8000"
+      ]
+
+      assert {:ok, pid} = start_supervised({UrlFailover, urls: urls, name: test})
+      assert is_pid(pid)
+    end
   end
 end
